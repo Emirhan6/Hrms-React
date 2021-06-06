77b06b33c016dfb4896f568a82ff7cdee94acab5
Emirhan Aykanlı <eaykanli86@gmail.com>
2021-06-06 16:29:59 +0300

    11. Gün Ödev Commit 2

--- src/layouts/Dashboard.jsx
+++ src/layouts/Dashboard.jsx
@@ -1,18 +1,51 @@
-import React from 'react'
-import EmployeeList from '../pages/EmployeeList'
-import EmployerList from '../pages/EmployerList'
-import JobSeekerList from '../pages/JobSeekerList'
-import PositionList from '../pages/PositionList'
-import Navi from './Navi'
+import React from "react";
+import EmployeeList from "../pages/EmployeeList";
+import EmployerList from "../pages/EmployerList";
+import JobSeekerList from "../pages/JobSeekerList";
+import PositionList from "../pages/PositionList";
+import Categories from "./Categories";
+import { Grid, GridRow } from "semantic-ui-react";
 
 export default function Dashboard() {
-    return (
-        <div>
-            <Navi/>
-            <PositionList/>
-            <JobSeekerList/>
-            <EmployerList/>
-            <EmployeeList/>
-        </div>
-    )
+  return (
+    <div>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={4}>
+            <Categories />
+          </Grid.Column>
+          <Grid.Column width={12}>
+            <PositionList />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={4}></Grid.Column>
+          <Grid.Column width={12}>
+            <JobSeekerList />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={4}></Grid.Column>
+          <Grid.Column width={12}>
+            <EmployerList />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={4}></Grid.Column>
+          <Grid.Column width={12}>
+            <EmployeeList />
+          </Grid.Column>
+        </Grid.Row>
+      </Grid>
+    </div>
+  );
 }
